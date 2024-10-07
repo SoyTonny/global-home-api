ffe52f0881e1545ecec72fbf865abfe91a0fd496
Antony Corpas <antonydaniel2436@correo.com>
2024-10-07 17:02:19 -0500

    Correciones

--- src/models/Employee.js
+++ src/models/Employee.js
@@ -10,6 +10,11 @@ const Employee = sequelize.define('employee', {
         type: DataTypes.STRING,
         allowNull: false
     },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
     password: {
         type: DataTypes.STRING,
         allowNull: false
